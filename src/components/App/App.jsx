93716f7a89426f68a4b793fa55d82ca7cd217d34
Kritagya <kritagyakumra777@gmail.com>
2023-06-09 15:16:53 +0530

    Added Complete Project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ResultsContainer from '../ResultsContainer/ResultsContainer';
+import SearchBox from '../SearchBox/SearchBox';
+import Header from './../Header/Header';
+import './App.css';
+
+const name = require('@rstacruz/startup-name-generator');
+
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            headerText: 'Name It!',
+            headerExpanded: true,
+            suggestedNames: [],
+        };
+    }
+
+    handleInputChange = (inputText) => {
+        this.setState({
+            headerExpanded: !inputText,
+            // headerExpanded: !(inputText.length > 0),
+            // headerExpanded: inputText.length > 0 ? false : true,
+            suggestedNames: inputText ? name(inputText) : [],
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <Header
+                    headerExpanded={this.state.headerExpanded}
+                    headTitle={this.state.headerText}
+                />
+                <SearchBox onInputChage={this.handleInputChange} />
+                <ResultsContainer suggestedNames={this.state.suggestedNames} />
+            </div>
+        );
+    }
+}
+
+export default App;
